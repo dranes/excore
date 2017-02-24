a3393cdd619e7627c8832133409c081ed7483ab0
Andres Garcia <andresgarcia@gmail.com>
2017-02-23 21:08:02 -0500

    Inital Project with select games UI
    Signed-off-by: Andres Garcia <andresgarcia@gmail.com>

---
+++ excore WatchKit Extension/PingPongController.swift
@@ -0,0 +1,31 @@
+//
+//  PingPongController.swift
+//  excore
+//
+//  Created by Dranes on 2/23/17.
+//  Copyright © 2017 Nexogy. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class PingPongController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ excore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  excore
+//
+//  Created by Dranes on 2/23/17.
+//  Copyright © 2017 Nexogy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
